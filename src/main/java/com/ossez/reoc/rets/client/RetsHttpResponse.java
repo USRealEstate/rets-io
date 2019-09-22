3ef2e82f63b44077afebe14793556026664417d5
YuCheng Hu <yhu@ossez.com>
2019-09-22 10:10:55 -0400

    REOC-59 Upgrade the Junit version to 4.12

--- src/main/java/com/ossez/reoc/rets/client/RetsHttpResponse.java
+++ src/main/java/com/ossez/reoc/rets/client/RetsHttpResponse.java
@@ -1,27 +1,26 @@
 package com.ossez.reoc.rets.client;
 
-import java.util.Map;
 import java.io.InputStream;
+import java.util.Map;
 
 /**
  * Interface for retrieving useful header fields from a RETS HTTP response
- * 
  *
+ * @author YuCheng Hu
  */
-
 public interface RetsHttpResponse {
-	public int getResponseCode() throws RetsException;
+    public int getResponseCode() throws RetsException;
 
-	public Map getHeaders() throws RetsException;
+    public Map getHeaders() throws RetsException;
 
-	public String getHeader(String hdr) throws RetsException;
+    public String getHeader(String hdr) throws RetsException;
 
-	public String getCookie(String cookie) throws RetsException;
+    public String getCookie(String cookie) throws RetsException;
 
-	public String getCharset() throws RetsException;
-	
-	public InputStream getInputStream() throws RetsException;
+    public String getCharset() throws RetsException;
 
-	public Map getCookies() throws RetsException;
+    public InputStream getInputStream() throws RetsException;
+
+    public Map getCookies() throws RetsException;
 
 }
