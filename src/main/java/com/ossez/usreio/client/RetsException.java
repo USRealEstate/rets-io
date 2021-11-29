986a1dc0fa5f1f933963f8e711f02157c970f696
YuCheng Hu <huyuchengus@gmail.com>
2021-11-29 16:00:22 -0500

    USRE-86 change the name of package

---
+++ src/main/java/com/ossez/usreio/client/RetsException.java
@@ -0,0 +1,22 @@
+package com.ossez.usreio.client;
+
+/**
+ * @author YuCheng Hu
+ */
+public class RetsException extends Exception {
+	public RetsException() {
+		super();
+	}
+
+	public RetsException(String message) {
+		super(message);
+	}
+
+	public RetsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public RetsException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/test/java/com/ossez/usreio/common/metadata/attrib/AttrGenericTextTest.java
@@ -0,0 +1,17 @@
+package com.ossez.usreio.common.metadata.attrib;
+
+import com.ossez.usreio.common.metadata.AttrType;
+
+public class AttrGenericTextTest extends AttrTypeTest {
+	public void testAttrGeneric() throws Exception {
+		AttrType parser = new AttrGenericText(0, 10, "abcdefg");
+
+		assertEquals("aaaaa", parser.parse("aaaaa",true));
+		assertEquals("abcdefg", parser.parse("abcdefg",true));
+		assertEquals("", parser.parse("",true));
+
+		assertParseException(parser, "abcdefG");
+		assertParseException(parser, "A");
+		assertParseException(parser, "abcdefgabcd");
+	}
+}
