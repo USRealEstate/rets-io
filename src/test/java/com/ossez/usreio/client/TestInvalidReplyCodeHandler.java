3f43210bff731cadb285b6369c551207250239d2
YuCheng Hu <huyuchengus@gmail.com>
2021-11-29 16:00:22 -0500

    USRE-86 change the name of package

---
+++ src/test/java/com/ossez/usreio/client/TestInvalidReplyCodeHandler.java
@@ -0,0 +1,17 @@
+package com.ossez.usreio.client;
+
+final class TestInvalidReplyCodeHandler implements InvalidReplyCodeHandler {
+    private int replyCode;
+    
+    public void invalidRetsReplyCode(int code) throws InvalidReplyCodeException {
+        throw new InvalidReplyCodeException(code);
+    }
+    
+    public void invalidRetsStatusReplyCode(int code) throws InvalidReplyCodeException {
+        this.replyCode = code;
+    }
+
+    public int getReplyCode() {
+        return this.replyCode;
+    }
+}
