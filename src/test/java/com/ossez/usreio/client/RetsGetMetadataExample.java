0a105fce11cdce5cad48d35efb5690fe752be546
YuCheng Hu <huyuchengus@gmail.com>
2021-11-29 17:45:07 -0500

    USRE-85 Change Test for session test

--- src/test/java/com/ossez/usreio/client/RetsGetMetadataExample.java
+++ src/test/java/com/ossez/usreio/client/RetsGetMetadataExample.java
@@ -9,64 +9,60 @@ import com.ossez.usreio.client.*;
 
 /**
  * Simple Example performing a GetMetadata and iterating of the results
- *
  */
 public class RetsGetMetadataExample {
 
-	public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException {
 
-		//Create a RetsHttpClient (other constructors provide configuration i.e. timeout, gzip capability)
-		RetsHttpClient httpClient = new CommonsHttpClient();
-		RetsVersion retsVersion = RetsVersion.RETS_1_7_2;
-		String loginUrl = "http://neren.rets.paragonrels.com/rets/fnisrets.aspx/NEREN/login";
+        //Create a RetsHttpClient (other constructors provide configuration i.e. timeout, gzip capability)
+        RetsHttpClient httpClient = new CommonsHttpClient();
+        RetsVersion retsVersion = RetsVersion.RETS_1_7_2;
+        String loginUrl = "http://neren.rets.paragonrels.com/rets/fnisrets.aspx/NEREN/login?rets-version=rets/1.7.2";
 
-		//Create a RetesSession with RetsHttpClient
-		RetsSession session = new RetsSession(loginUrl, httpClient, retsVersion);
+        //Create a RetesSession with RetsHttpClient
+        RetsSession session = new RetsSession(loginUrl, httpClient, retsVersion);
 
-		String username = "prurets1";
-		String password = "boyd070110";
+        String username = "625166idx";
+        String password = "B5R6PZ2KGN3eS5m7";
 
-		//Set method as GET or POST
-		session.setMethod("POST");
-		try {
-			//Login
-			LoginResponse loginResponse = session.login(username, password);
+        //Set method as GET or POST
+        session.setMethod("POST");
+        try {
+            //Login
+            LoginResponse loginResponse = session.login(username, password);
 
-			System.out.println(">>>" + loginResponse.getSessionId());
-		} catch (RetsException e) {
-			e.printStackTrace();
-		}
+            System.out.println(">>>" + loginResponse.getSessionId());
+        } catch (RetsException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			MSystem system = session.getMetadata().getSystem();
-			System.out.println(
-					"SYSTEM: " + system.getSystemID() + 
-					" - " + system.getSystemDescription());
+        try {
+            MSystem system = session.getMetadata().getSystem();
+            System.out.println(
+                    "SYSTEM: " + system.getSystemID() +
+                            " - " + system.getSystemDescription());
 
-			for(MResource resource: system.getMResources()) {
+            for (MResource resource : system.getMResources()) {
 
-				System.out.println(
-						"    RESOURCE: " + resource.getResourceID());
+                System.out.println(
+                        "    RESOURCE: " + resource.getResourceID());
 
-				for(MClass classification: resource.getMClasses()) {
-					System.out.println(
-							"        CLASS: " + classification.getClassName() +
-							" - " + classification.getDescription());
-				}
-			}
-		}
-		catch (RetsException e) {
-			e.printStackTrace();
-		} 	
-		finally {
-			if(session != null) { 
-				try {
-					session.logout(); 
-				} 
-				catch(RetsException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}	
+                for (MClass classification : resource.getMClasses()) {
+                    System.out.println(
+                            "        CLASS: " + classification.getClassName() +
+                                    " - " + classification.getDescription());
+                }
+            }
+        } catch (RetsException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                try {
+                    session.logout();
+                } catch (RetsException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
