7297ffb825790b1372feca1cc469a63cc028d4d4
YuCheng Hu <huyuchengus@gmail.com>
2021-11-29 16:00:22 -0500

    USRE-86 change the name of package

--- src/main/java/com/ossez/reoc/rets/client/LogoutRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ossez.reoc.rets.client;
-
-public class LogoutRequest extends VersionInsensitiveRequest {
-
-	@Override
-	public void setUrl(CapabilityUrls urls) {
-		setUrl(urls.getLogoutUrl());
-	}
-}
---
+++ src/main/java/com/ossez/usreio/client/InvalidArgumentException.java
@@ -0,0 +1,7 @@
+package com.ossez.usreio.client;
+
+public class InvalidArgumentException extends RetsException {
+	public InvalidArgumentException(String message) {
+		super(message);
+	}
+}
