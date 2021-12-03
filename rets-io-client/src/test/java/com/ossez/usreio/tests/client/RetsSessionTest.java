8a62ee68f093f308463871057ab2ada90e74046d
YuCheng Hu <huyuchengus@gmail.com>
2021-12-03 09:15:06 -0500

    USRE-72 Change the name for Session testing

--- rets-io-client/src/test/java/com/ossez/usreio/tests/client/RetsSessionTest.java
+++ rets-io-client/src/test/java/com/ossez/usreio/tests/client/RetsSessionTest.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
- * Test for RETS session
+ * RETS Connection Test
  *
  * @author YuCheng Hu
  */
